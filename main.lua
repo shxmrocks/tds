784ad2d02fa89713ca3d79b3425878f088c3d50a
shxmrocks <utsumiiaoba@gmail.com>
2026-07-05 16:02:06 -0400

    fix logging upgrades

--- main.lua
+++ main.lua
@@ -632,7 +632,7 @@ function Tower:Upgrade(PathId: number): boolean
     end)
 
     if ok and checkOk(res) then 
-        Log(string.format("Successfully upgraded %s at index #%s", self.Replicator:GetAttribute("Name"), index), Color3.new(0, 1, 0))
+        Log(string.format("Successfully upgraded %s at index #%s", self.Replicator:GetAttribute("Name"), self.TDS.PlacedTowers[self]), Color3.new(0, 1, 0))
         return true
     else
         return Log(msg, Color3.new(1, 1, 0))
